a17a0ee652baf9627b7516e14435b56ca871ac51
inc-Cuong-Nguyen <icn.cuong.nguyen@gmail.com>
2019-01-02 01:28:33 +0700

    Commiting changes

--- data.php
+++ data.php
@@ -3,8 +3,28 @@
 include './config.php';
 
 class data{
+    
+    private $conn;
+            
+    
     function __construct() {
-        ;
+       $this->conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+       mysqli_set_charset($this->con,"utf8");
     }
+    
+    public function _getprovinces(){
+        $qery = 'select * from provinces '.$this->_orderbyDESC('idp');
+        $datas = mysqli_query($this->conn, $qery);
+        return $datas;
+    }
+    
+    private function _orderbyDESC($fill){
+        return 'order by '.$fill.' '.'DESC';
+    }
+    
+     private function _orderbyASC($fill){
+        return 'order by '.$fill.' '.'ASC';
+    }
+    
 }
 
